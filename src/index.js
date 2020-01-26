d1d4ceaca07c7a67abe486f2489493b0e07fef85
i.m.a.1988@gmail.com <i.m.a.1988@gmail.com>
2020-01-26 16:02:27 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore, bindActionCreators } from 'redux';
+
+import Counter from './counter';
+
 import reducer from './reducer';
 import * as actions from './actions';
 
@@ -9,33 +14,48 @@ const { dispatch } = store;
 const { inc, dec, rnd } =
   bindActionCreators(actions, dispatch);
 
-document
-  .getElementById('inc')
-  .addEventListener('click', inc);
-  // .addEventListener('click', () => {
-  //   store.dispatch({type: 'INC'});
-  // });
+// document
+//   .getElementById('inc')
+//   .addEventListener('click', inc);
+//   // .addEventListener('click', () => {
+//   //   store.dispatch({type: 'INC'});
+//   // });
 
-document
-  .getElementById('dec')
-  .addEventListener('click', dec);
-  // .addEventListener('click', () => {
-  //   store.dispatch({type: 'DEC'});
-  // });
+// document
+//   .getElementById('dec')
+//   .addEventListener('click', dec);
+//   // .addEventListener('click', () => {
+//   //   store.dispatch({type: 'DEC'});
+//   // });
 
-document
-  .getElementById('rnd')
-  .addEventListener('click', () => {
-    const payload =  Math.floor(Math.random()*10);
-    rnd(payload);
-    //store.dispatch({ type: 'RND', payload });
-  });
+// document
+//   .getElementById('rnd')
+//   .addEventListener('click', () => {
+//     const payload =  Math.floor(Math.random()*10);
+//     rnd(payload);
+//     //store.dispatch({ type: 'RND', payload });
+//   });
+
+// const update = () => {
+//   document
+//     .getElementById('counter')
+//     .innerHTML = store.getState();
+// };
 
 const update = () => {
-  document
-    .getElementById('counter')
-    .innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random()*10);
+        rnd(value);
+      }}/>,
+    document.getElementById('root'));
 };
+
+update();
 
 store.subscribe(update);
 
